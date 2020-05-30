44623d4e4434637baa7a8c1a28b95efc49ee0852
zhengwenbin <952627474@qq.com>
2020-05-30 17:25:18 +0800

    777

--- src/common/Sidebar.vue
+++ src/common/Sidebar.vue
@@ -110,6 +110,7 @@ export default {
   },
   created() {
     // 通过 Event Bus 进行组件间通信，来折叠侧边栏
+    // 77777777
     bus.$on("collapse", msg => {
       this.collapse = msg;
       bus.$emit("collapse-content", msg);
